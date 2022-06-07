f888c723a43f23c5068ca1b99ad7ad2bf869551b
Elementary1092 <pdead6223@gmail.com>
2022-06-07 22:17:53 +0500

    Add link to the Github

--- src/main/java/com/task4/demo/common/DefaultFooterConfigurer.java
+++ src/main/java/com/task4/demo/common/DefaultFooterConfigurer.java
@@ -6,6 +6,6 @@ public class DefaultFooterConfigurer implements FooterConfigurer {
     @Override
     public void populateFooter(Model model) {
         model.addAttribute("email", "mailto: sardorjuraboev1092gmail.com");
-        model.addAttribute("github", "https://github.com");
+        model.addAttribute("github", "https://github.com/Elementary1092/task4");
     }
 }
